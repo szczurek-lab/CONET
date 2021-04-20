a4714199b0e66e7d3493576de28f3ab93dc1cd4b
tc360950 <tc360950@students.mimuw.edu.pl>
2021-04-20 13:45:00 +0200

    fixes in Data COnverter

--- python/conet/src/data_converter/data_converter.py
+++ python/conet/src/data_converter/data_converter.py
@@ -46,7 +46,9 @@ class DataConverter:
             self.__add_chromosome_ends()
 
     # indices must be numbered from 0
-    def create_CoNET_input_files(self, loci_candidate_indices: list, out_path: str, chromosomes=None):
+    def create_CoNET_input_files(self, loci_candidate_indices: list, out_path: str, add_chr_ends_to_indices=False, chromosomes=None):
+        if add_chr_ends_to_indices:
+            loci_candidate_indices = self.__add_chromosome_ends_to_indices(loci_candidate_indices)
         if chromosomes is not None:
             loci_candidate_indices = self.__filter_loci_to_chromosomes(loci_candidate_indices, chromosomes)
 
@@ -101,7 +103,6 @@ class DataConverter:
                 bins_lengths += self.__get_bin_length(j)
                 j = j + 1
             diffs[i, 2] = bins_lengths / self.event_length_normalizer
-        self.__get_sum_of_counts(1, 10)
         return np.transpose(diffs)
 
     def __get_sum_of_counts(self, left_loci, right_loci):
@@ -134,24 +135,35 @@ class DataConverter:
         return np.transpose(counts), np.transpose(squared_counts)
 
     def __add_chromosome_ends(self):
-        sum = 0
-        for x in self.human_chr_lengths.values():
-            sum = sum + x
-        print(sum)
         i = 0
         while i < self.corrected_counts.shape[0]:
             loci_chr = self.__get_loci_chromosome(i)
             if i == self.corrected_counts.shape[0] - 1 or loci_chr != self.__get_loci_chromosome(i + 1):
+                print(i)
                 line = np.full(self.corrected_counts.shape[1], self.NEUTRAL_CN)
                 line[self.CHROMOSOME_COLUMN] = loci_chr
                 line[self.BIN_START_COLUMN] = self.human_chr_lengths[loci_chr]
                 line[self.BIN_START_COLUMN + 1] = self.human_chr_lengths[loci_chr] + self.default_bin_length
                 line[self.BIN_START_COLUMN + 2] = self.default_bin_length
-                line = np.transpose(line)
-                dummy_line = pd.DataFrame({"chr": loci_chr}, index=[i + 1])
-                self.corrected_counts = pd.concat([self.corrected_counts.iloc[:(i+1)], dummy_line, self.corrected_counts.iloc[(i+1):]]).reset_index(drop=True)
-                self.corrected_counts.iloc[i+1] = line
+                chunk = self.corrected_counts.iloc[:(i+1)].append(self.corrected_counts.iloc[i], ignore_index=True)
+                chunk.iloc[i+1] = line
+                self.corrected_counts = chunk.append(self.corrected_counts.iloc[(i+1):], ignore_index=True)
                 i = i + 2
             else:
                 i = i + 1
         self.LOCI_COUNT = self.corrected_counts.shape[0]
+
+    def __add_chromosome_ends_to_indices(self, loci_indices):
+        i = 0
+        chr_ends = []
+        while i < self.LOCI_COUNT:
+            if i == self.LOCI_COUNT - 1 or self.__get_loci_chromosome(i) != self.__get_loci_chromosome(i + 1):
+                after_end = [loci for loci in loci_indices if loci >= i]
+                before_end = [loci for loci in loci_indices if loci < i]
+                after_end = list(map(lambda x: x + 1, after_end))
+                loci_indices = before_end + after_end
+                chr_ends.append(i)
+            i = i +1
+        loci_indices = loci_indices + chr_ends
+        loci_indices.sort()
+        return loci_indices
