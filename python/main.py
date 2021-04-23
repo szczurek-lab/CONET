8c213fd87389e00461c5a677bed47ed94a3fe837
tc360950 <37670208+tc360950@users.noreply.github.com>
2021-04-23 11:09:11 +0200

    Delete main.py

--- python/main.py
+++
@@ -1,9 +0,0 @@
-import conet.src.data_converter.data_converter as dc
-import pandas
-
-x = dc.DataConverter("notebooks/data/SA501X3F_filtered_corrected_counts.csv", ',', 150000, 3095677412, True)
-y = x.corrected_counts
-
-df = pandas.read_csv('indeksy', header=None)[0].tolist()
-x.create_CoNET_input_files(df, "./")
-print("dupa")
