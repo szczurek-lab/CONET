2f8ee4b20ac3c2d425e7890d5713e9669d3849b2
tcakala <tcakala@deloittece.com>
2022-05-21 12:02:33 +0200

    Updates in notebooks

--- python/conet-py/conet/conet_parameters.py
+++ python/conet-py/conet/conet_parameters.py
@@ -1,54 +1,26 @@
+from dataclasses import dataclass
 
 
+@dataclass
 class CONETParameters:
-    #
-    # @data_dir - path to output directory
-    # @param_inf_iters - number of iterations for parameters inference
-    # @pt_inf_iters - number of iterations for tree search with parallel tempering
-
-    #
-    # @mixture_size - maximal size of mixture which defines difference distribution
-    # @num_replicas - number of replicas in parallel tempering
-    # @threads_likelihood - number of threads which shall be used in likelihood calculation during parameter inference
-    #
-    def __init__(self,
-                 data_dir="./",
-                 param_inf_iters=100000,
-                 pt_inf_iters=100000,
-                 counts_penalty_s1=0.0,
-                 counts_penalty_s2=0.0,
-                 event_length_penalty_k0=1.0,
-                 tree_structure_prior_k1=1.0,
-                 use_event_lengths_in_attachment=True,
-                 seed=12312,
-                 mixture_size=4,
-                 num_replicas=5,
-                 threads_likelihood=4,
-                 parameter_resampling_frequency=10,
-                 moves_between_swaps=10,
-                 burn_in=10000,
-                 verbose=True,
-                 neutral_cn=2.0,
-                 output_dir="./"
-                 ):
-        self.data_dir = data_dir
-        self.param_inf_iters = param_inf_iters
-        self.pt_inf_iters = pt_inf_iters
-        self.counts_penalty_s1 = counts_penalty_s1
-        self.counts_penalty_s2 = counts_penalty_s2
-        self.event_length_penalty_k0 = event_length_penalty_k0
-        self.tree_structure_prior_k1 = tree_structure_prior_k1
-        self.use_event_lengths_in_attachment = (lambda x: 1 if x else 0)(use_event_lengths_in_attachment)
-        self.seed = seed
-        self.mixture_size = mixture_size
-        self.num_replicas = num_replicas
-        self.threads_likelihood = threads_likelihood
-        self.parameter_resampling_frequency = parameter_resampling_frequency
-        self.moves_between_swaps = moves_between_swaps
-        self.burn_in = burn_in
-        self.verbose = (lambda x: 1 if x else 0)(verbose)
-        self.neutral_cn = neutral_cn
-        self.output_dir = output_dir
+    data_dir: str = "./"
+    param_inf_iters: int = 100000
+    pt_inf_iters: int = 100000
+    counts_penalty_s1: float = 0.0
+    counts_penalty_s2: float = 0.0
+    event_length_penalty_k0: float = 1.0
+    tree_structure_prior_k1: float = 1.0
+    use_event_lengths_in_attachment: bool = True
+    seed: int = 12312
+    mixture_size: int = 4
+    num_replicas: int = 5
+    threads_likelihood: int = 4
+    parameter_resampling_frequency: int = 10
+    moves_between_swaps: int = 10
+    burn_in: int = 10000
+    verbose: bool = True
+    neutral_cn: float = 2.0
+    output_dir: str = "./"
 
     def to_string(self):
         return [
@@ -59,7 +31,7 @@ class CONETParameters:
             str(self.counts_penalty_s2),
             str(self.event_length_penalty_k0),
             str(self.tree_structure_prior_k1),
-            str(self.use_event_lengths_in_attachment),
+            str((lambda x: 1 if x else 0)(self.use_event_lengths_in_attachment)),
             str(self.seed),
             str(self.mixture_size),
             str(self.num_replicas),
@@ -67,6 +39,6 @@ class CONETParameters:
             str(self.parameter_resampling_frequency),
             str(self.moves_between_swaps),
             str(self.burn_in),
-            str(self.verbose),
+            str((lambda x: 1 if x else 0)(self.verbose)),
             str(self.neutral_cn),
             str(self.output_dir)]
